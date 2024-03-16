49ac5fa9366a68439930c886cb284a6cf298ae50
Anton Ivarsson <antonivarson91@gmail.com>
2024-03-16 16:20:19 +0100

    GrapheneOS MD

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+st.markdown ("""### GrapheneOS
+
+- [Full Setup Guide, maximise Privacy, Security & Battery after Installation](https://youtu.be/dPXu-XKxBT4) - author's youtube: [DigitalIndependent](https://www.youtube.com/@DigitalIndependent)
+
+#### Which devices are **supported**?
+GrapheneOS has official production support for the following devices:
+
+- Pixel 8 Pro (husky)
+- Pixel Fold (felix)
+- Pixel 8 (shiba)
+- Pixel Tablet (tangorpro)
+- Pixel 7a (lynx)
+- Pixel 7 Pro (cheetah)
+- Pixel 7 (panther)
+- Pixel 6a (bluejay)
+- Pixel 6 Pro (raven)
+- Pixel 6 (oriole)
+- Pixel 5a (barbet)
+
+The following devices are end-of-life, no longer receive firmware or most driver security updates and receive extended support from GrapheneOS as part of the main releases with all GrapheneOS changes including all of the latest Android Open Source Project changes:
+
+- Pixel 5 (redfin)
+- Pixel 4a (5G) (bramble)
+
+The following devices are end-of-life, no longer receive firmware or driver security updates and receive extended support from GrapheneOS via a legacy branch based on Android 13 with only the Android Open Source Project security backports, certain other security patches and other minimal changes to keep them working:
+
+- Pixel 4a (sunfish)
+- Pixel 4 XL (coral)
+- Pixel 4 (flame)
+
+#### Summary
+Graphene OS has several important features that users may be missing out on if they stick to the default settings. This video script provides a checklist for installing new Graphene OS phones and creating user profiles to maximize privacy and security.
+
+#### Highlights
+
+- User profiles in Graphene OS allow for better privacy and security by isolating different parts of your digital life.
+- 2:22 ⚠️ Creating separate profiles is important to prevent misbehaving or malicious apps from affecting your main profile.
+- 4:10 ✅ User profiles can be easily activated in the settings, and you can even migrate existing apps to new profiles.
+- 5:43 🔒 Having separate profiles can be useful in situations where you need to give your phone to someone else but want to protect your private data.
+- 8:15 📶 Recommended network and Wi-Fi settings include turning off automatic Wi-Fi scanning and changing hotspot names and passwords.
+- 10:18 📱 Changing device names and adjusting Bluetooth settings are important for privacy and convenience.
+- 12:10 🚫 Disabling NFC and cloud printing can enhance security and prevent potential vulnerabilities.
+- 14:07 📵 Adjusting notification settings, battery percentage display, and enabling dual Scopes are important for a personalized and secure experience.
+- 16:55 🔒 Privacy settings include managing sensor permissions, camera and microphone access, and clipboard visibility.
+- 19:54 🔐 Choosing between PIN and fingerprint security depends on threat models and personal preferences.""")
